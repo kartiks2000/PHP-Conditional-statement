8c7a6a4121abc06a9d1e4236c8a7191dc498b051
kartiks2000 <46376692+kartiks2000@users.noreply.github.com>
2020-08-02 12:54:22 +0530

    Add files via upload

---
+++ dem.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>If ,If-else ,If-elseif-else statements in PHP</h1>
+    <?php 
+        // If ,If-else ,If-elseif-else statements
+
+        // If
+        if(2>3){
+            echo "yes it is true";
+        }
+
+
+        // If-else
+        if(7>3){
+            echo "yes its true";
+        }
+        else{
+            echo "Nah not true";
+        }
+
+
+        // If-elseif-else
+        if(4>6){
+            echo "true";
+        }
+        elseif(7>8){
+            echo "maybe";
+        }
+        else{
+            echo "nah not true";
+        }
+        
+        
+    ?>
+</body>
+</html>
